fabcecc793cf0aaa8ef236ce4c83c4282aaa0650
JaquelyneMendezHernandez <88173967+JaquelyneMendezHernandez@users.noreply.github.com>
2023-05-23 17:53:56 -0600

    correciones

--- ByC/assets/js/script.js
+++ ByC/assets/js/script.js
@@ -5,8 +5,8 @@
     function isElementInViewport(el){
         var rect = el.getBoundingClientRect();
         return(
-            rect.top >= 0&&
-            rect.left >= 0&&
+            rect.top >= 0 &&
+            rect.left >= 0 &&
             rect.bottom <=
             (window.innerHeight || document.documentElement.clientHeight) &&
             rect.right <= (window.innerWidth || document.documentElement.clientWidth)
@@ -14,7 +14,7 @@
     }
 
     function callbackFunc(){
-        for(let i=0; i<item.length; i++){
+        for(let i=0; i< item.length; i++){
             if(isElementInViewport(item[i])){
                 item[i].classList.add('in-view')
             }
